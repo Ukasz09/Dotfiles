6eddd5139ea2aaffbf10f37387915a1a9ff6b56a
Ukasz09 <246703@student.pwr.edu.pl>
2020-05-31 14:38:14 +0200

    Add new two bin files; add aliases

--- aliases/.bash_aliases
+++ aliases/.bash_aliases
@@ -1,4 +1,11 @@
 alias pdftext="lowriter --convert-to pdf"
 alias listen-ports="sudo lsof -i -P -n | grep LISTEN"
-alias git-log="git log --oneline"
 alias path="echo -e ${PATH//:/\\\\n}"
+alias sys-serv="systemctl list-unit-files"
+alias tardo="tar -czvf"
+alias untardo="tar -xzvf"
+
+# git
+alias gitlog="git log --oneline"
+alias gitstat="git status -u"
+
--- bin_maker.sh
+++ bin_maker.sh
@@ -5,15 +5,24 @@ readonly USERNAME=ukasz09 # because normal $USERNAME and ~ dont work in script
 make_yt_dir() {
     if [ ! -d "/home/$USERNAME/Wideo/YouTube" ]; then
         mkdir /home/$USERNAME/Wideo/YouTube
-        echo "Making yt dir"
+        echo "Making YouTube/ dir"
     fi
 }
 
+make_websites_dir() {
+    if [ ! -d "/home/$USERNAME/Dokumenty/Websites" ]; then
+        mkdir /home/$USERNAME/Dokumenty/Websites
+        echo "Making Websites/ dir"
+    fi
+}
+
+ 
 # -------------------------------------------------------------------------------------------------------------------- #
 copy_bin() {
     cd ./bin
     cp -i * /usr/bin/
 }
 
+make_websites_dir
 make_yt_dir
 copy_bin
